01ff068db9f1bac5896e0700a4d289a80af4b0e3
yiren <renyi.tsinghua@gmail.com>
2015-03-07 22:14:45 -0700

    changed io.on to io.sockets.on in io.js

--- io.js
+++ io.js
@@ -1,8 +1,11 @@
 var io = require('socket.io')();
 
 io.sockets.on('connection', function(socket){
+    console.log('connected');
     socket.on('selection', function(sig){
+        console.log('selection received...');
         io.emit('selection', sig);
+        console.log('selection emitted...');
     });
 });
 
