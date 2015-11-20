8782e47d1fe263eb315eeaf11c37d1baa85a6091
hope-yao <hope-yao@asu.edu>
2015-11-19 22:33:08 -0700

    test touch swip

--- public/javascripts/gamescripts/game.js
+++ public/javascripts/gamescripts/game.js
@@ -764,6 +764,38 @@ var GAME = (function($){
 
             window.addEventListener( 'resize', App.onWindowResize, false );
 
+            window.addEventListener('load', function(){
+
+                var box1 = document.getElementById('box1')
+                var statusdiv = document.getElementById('statusdiv')
+                var startx = 0
+                var dist = 0
+
+                box1.addEventListener('touchstart', function(e){
+                    var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
+                    startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
+                    //statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px'
+                    $('#instruction p').html('Status: touchstart<br> ClientX: ' + startx + 'px');
+                    e.preventDefault()
+                }, false)
+
+                box1.addEventListener('touchmove', function(e){
+                    var touchobj = e.changedTouches[0] // reference first touch point for this event
+                    var dist = parseInt(touchobj.clientX) - startx
+                    //statusdiv.innerHTML = 'Status: touchmove<br> Horizontal distance traveled: ' + dist + 'px'
+                    $('#instruction p').html('Status: touchmove<br> Horizontal distance traveled: ' + dist + 'px');
+                    e.preventDefault()
+                }, false)
+
+                box1.addEventListener('touchend', function(e){
+                    var touchobj = e.changedTouches[0] // reference first touch point for this event
+                    //statusdiv.innerHTML = 'Status: touchend<br> Resting x coordinate: ' + touchobj.clientX + 'px'
+                    $('#instruction p').html('Status: touchend<br> Resting x coordinate: ' + touchobj.clientX + 'px');
+                    e.preventDefault()
+                }, false)
+
+            }, false);
+
             // Player
             App.$guessinput.on('keypress', App.onGuessinputKeyPress);
 
@@ -1294,6 +1326,10 @@ var GAME = (function($){
                     }
                 }
             }
+        },
+
+        touch_select: function(){
+            select();
         },
 
         /**
